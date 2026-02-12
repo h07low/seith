db4c2065fe5f722291c850a1fe2bd4f5e0bd5697
h07low <sigpwn@cssardinia.it>
2026-02-12 13:33:14 +0100

    structured commands and parser

--- src/seith_exec.py
+++ src/seith_exec.py
@@ -6,7 +6,7 @@ import logging
 import sys
 from pathlib import Path
 
-from seith_lib.commands import commands as seith_commands
+from seith_lib.commands import exec_parser, commands 
 from seith_lib.completers import completers 
 
 logger = logging.getLogger(__name__)
@@ -18,22 +18,12 @@ def log():
 
 def parse_args():
     container_name = Path(sys.argv[0]).stem
-    # exec parser #TODO interactive mode
-    # TODO common function
+
     exec = argparse.ArgumentParser(prog = container_name, description='i cant explain TODO', epilog='GoodBye TODO')
-    exec.add_argument('--env', help='extra environment variable to pass to the command', default='{}')
-    exec.add_argument('--shell', help='specify shell to use (default=bash)', default='bash')
-    exec.add_argument('--cwd', help='specify directory to use (default=/seith_temp)', default=None)
-    exec.add_argument('--user', help='specify user to use (default=root)', default='root')
-    exec.add_argument('command', nargs='*', help='command to execute').completer = completers.docker_command
-    exec.set_defaults(func=seith_commands["exec"])
+    exec_parser(exec, 'exec', commands['exec']['help'], commands['exec'].get('func', 'exec')) 
     exec.set_defaults(container=container_name)
-    
-    #TODO eval "$(register-python-argcomplete blockchain)"
-    #TODO ln -s ~/.local/bin/seith_exec ~/.local/bin/blockchain
 
     argcomplete.autocomplete(exec)
-
     args = exec.parse_args()
 
     return args
--- src/seith_lib/commands.py
+++ src/seith_lib/commands.py
@@ -1,5 +1,7 @@
-# seith_lib/commands.py
+import argcomplete
 import importlib
+import argparse
+from seith_lib.completers import completers 
 
 def _lazy_load(command_name):
     """Lazy load and execute command"""
@@ -8,13 +10,74 @@ def _lazy_load(command_name):
         return module.run(args)
     return wrapper
 
-commands = {
-    "exec": _lazy_load("exec"),
-    "sync_deps": _lazy_load("sync_deps"),
-    "create": _lazy_load("create"),
-    "start": _lazy_load("start"),
-    "stop": _lazy_load("stop"),
-    "info": _lazy_load("info"),
-    "upgrade": _lazy_load("upgrade"),
-}
 
+
+
+def exec_parser(parent_parser, name, help, func_name):
+    if isinstance(parent_parser, argparse._SubParsersAction):
+        parser = parent_parser.add_parser(name, help=help)
+        parser.add_argument('container', help='container name', default='').completer = completers.containers
+    else:
+        parser = parent_parser
+
+    parser.add_argument('--env', help='extra environment variable to pass to the command', default='{}')
+    parser.add_argument('--shell', help='specify shell to use (default=bash)', default='bash')
+    parser.add_argument('--cwd', help='specify directory to use (default=/seith_temp)', default=None)
+    parser.add_argument('--user', help='specify user to use (default=root)', default='root')
+    parser.add_argument('command', nargs='*', help='command to execute').completer = completers.docker_command
+    parser.set_defaults(func=_lazy_load(func_name))
+    return parser
+
+def create_parser(parent_parser, name, help, func_name):
+    if isinstance(parent_parser, argparse._SubParsersAction):
+        parser = parent_parser.add_parser(name, help=help)
+    else:
+        parser = parent_parser
+
+    parser.add_argument('container', help='container name', default='').completer = completers.containers
+    parser.add_argument('--conf-dir', help='specify conf dir (default=XDG_CONFIG_HOME/seith/containers/<name>/config.json)') 
+    parser.set_defaults(func=_lazy_load(func_name))
+    return parser
+
+def info_parser(parent_parser, name, help, func_name):
+# subparser
+    if isinstance(parent_parser, argparse._SubParsersAction):
+        p = parent_parser.add_parser(name, help=help)
+
+    # defined parser
+    else:
+        p = parent_parser
+
+    p.add_argument('container', nargs="?", help='container name', default='').completer = completers.containers
+    p.set_defaults(func=_lazy_load(func_name))
+    return p
+
+
+def container_parser(parent_parser, name, help, func_name):
+    # subparser
+    if isinstance(parent_parser, argparse._SubParsersAction):
+        p = parent_parser.add_parser(name, help=help)
+
+    # defined parser
+    else:
+        p = parent_parser
+
+    p.add_argument('container', help='container name', default='').completer = completers.containers
+    p.set_defaults(func=_lazy_load(func_name))
+    return p
+
+
+def add_all_parsers(subparser):
+    for name, cmd in commands.items():
+        cmd['parser'](subparser, name, cmd['help'], cmd.get('func', name)) 
+
+commands = {
+    'create':   {"parser":create_parser,    "help":"create container"},
+    'info':     {"parser":info_parser,      "help":"get containers info"},
+    'start':    {"parser":container_parser, "help":"start container"},
+    'stop':     {"parser":container_parser, "help":"stop container"},
+    'sync_deps':{"parser":container_parser, "help":"install added deps"},
+    'upgrade':  {"parser":container_parser, "help":"upgade installed deps"},
+    
+    'exec':     {"parser":exec_parser,       "help":"exec commands on container"},
+}
